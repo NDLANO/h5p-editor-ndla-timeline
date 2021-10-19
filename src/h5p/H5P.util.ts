5ef08207a84af5d66c5327447302f581c53f5e9a
Kent Inge F. Simonsen <40064+kentis@users.noreply.github.com>
2021-10-19 14:15:00 +0200

    Update src/h5p/H5P.util.ts
    Co-authored-by: Sindre Bøyum <boyum@users.noreply.github.com>

--- src/h5p/H5P.util.ts
+++ src/h5p/H5P.util.ts
@@ -2,4 +2,4 @@ import { H5PObject, H5PEditorObject } from "../../H5P";
 
 export const H5P = (window as any).H5P as H5PObject;
 export const H5PEditor = (window as any).H5PEditor as H5PEditorObject;
-export const t = H5PEditor.t.bind(null, "H5PEditor.Timeline");
+export const t: (translationKey: keyof typeof import("../../language/en.json").libraryStrings) => string = H5PEditor.t.bind(null, "H5PEditor.Timeline");
